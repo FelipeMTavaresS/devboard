89c481d9bc5c5adfff631a9ee2788fc5689e6c38
Felipe Tavares <162457368+FelipeMTavaresS@users.noreply.github.com>
2026-03-10 15:14:07 -0300

    Initialize server for e2e tests
    Added server initialization for HTTP server in tests.

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { ApiModule } from './../src/api.module';
+import { Server } from 'http';
 
 describe('ApiController (e2e)', () => {
   let app: INestApplication;
@@ -16,6 +17,7 @@ describe('ApiController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    const server = app.getHttpServer() as Server:
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
